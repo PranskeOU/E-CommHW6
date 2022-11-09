b1a1fb53ac0fcba3c2bf604ff38608378c682995
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-09 13:59:22 -0600

    Update library1.php

--- library1.php
+++ library1.php
@@ -1,2 +1,49 @@
 <?php require_once("header.php"); ?>
-<h4>Library 1</h4>
+<script src="https://d3js.org/d3.v7.min.js">
+chart = {
+  const svg = d3.create("svg")
+      .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+  const chords = chord(matrix);
+
+  const group = svg.append("g")
+      .attr("font-size", 10)
+      .attr("font-family", "sans-serif")
+    .selectAll("g")
+    .data(chords.groups)
+    .join("g");
+
+  group.append("path")
+      .attr("fill", d => color(names[d.index]))
+      .attr("d", arc);
+
+  group.append("text")
+      .each(d => (d.angle = (d.startAngle + d.endAngle) / 2))
+      .attr("dy", "0.35em")
+      .attr("transform", d => `
+        rotate(${(d.angle * 180 / Math.PI - 90)})
+        translate(${outerRadius + 5})
+        ${d.angle > Math.PI ? "rotate(180)" : ""}
+      `)
+      .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
+      .text(d => names[d.index]);
+
+  group.append("title")
+      .text(d => `${names[d.index]}
+${d3.sum(chords, c => (c.source.index === d.index) * c.source.value)} outgoing →
+${d3.sum(chords, c => (c.target.index === d.index) * c.source.value)} incoming ←`);
+
+  svg.append("g")
+      .attr("fill-opacity", 0.75)
+    .selectAll("path")
+    .data(chords)
+    .join("path")
+      .style("mix-blend-mode", "multiply")
+      .attr("fill", d => color(names[d.target.index]))
+      .attr("d", ribbon)
+    .append("title")
+      .text(d => `${names[d.source.index]} → ${names[d.target.index]} ${d.source.value}`);
+
+  return svg.node();
+}
+</script>
