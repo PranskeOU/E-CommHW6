b4da1460ba6c4b3f08904bdcc5e54a9d9994b1e3
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-15 15:14:47 -0600

    Update library2.php

--- library2.php
+++ library2.php
@@ -1,18 +1,9 @@
 <?php require_once("header.php"); ?>
 <head>
+  <script src="library2script.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
   <h4 style="text-align:center; color:white;" id="hidden">This is the second library. This text was hidden until you hovered over it, but it will soon disappear forever.</h4>
-  <script>
-    $(document).ready(function(){
-      $("#hidden").hover(function(){
-      $(this).css("color","black");
-        },
-    function(){
-      $(this).hide();
-      }
-    }
-  </script>
 </body>
 <h4>Library 2</h4>
