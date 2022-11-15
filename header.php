5648956305b1f6bf5d07c2a16bb63b4879c21858
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-15 15:15:56 -0600

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HW 6: JavaScript Libraries</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="library2script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg bg-light">
