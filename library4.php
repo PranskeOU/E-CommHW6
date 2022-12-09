5e7470c7b250921c9dbac4b1237379a40ad96e89
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-09 14:53:47 -0600

    Update library4.php

--- library4.php
+++ library4.php
@@ -1,2 +1,55 @@
 <?php require_once("header.php"); ?>
-<h4>Library 4</h4>
+<body>
+
+    <nav>
+                <a href="index.html">Home</a>
+                <a href="lib1.html">Lib 1</a>
+                <a href="lib2.html">Lib 2</a>
+            </nav>
+
+
+   <script src="//cdn.rawgit.com/mrdoob/three.js/master/build/three.min.js"></script>
+		<script>
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry(200, 200, 200);
+    material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+}
+
+function animate() {
+
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+
+}
+			      </script><canvas data-engine="three.js r147" width="725" height="569" style="display: block; width: 725px; height: 569px;"></canvas>
+			     
+
+</body>
